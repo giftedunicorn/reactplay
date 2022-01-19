02e6eb992cc92a5dc1866066cb32ac101805c460
ednew2018 <shenjian8628@gmail.com>
2022-01-19 16:55:26 +0800

    Update and fix modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,19 +4,14 @@ import './../styles/Modal.css';
 function Modal(props) {
   const modalRef = useRef(null);
   let keydownCloseEvent = null
-  const mountedStyle = { animation: "inAnimation 250ms ease-in" };
-  const unmountedStyle = {
-    animation: "outAnimation 270ms ease-out",
-    animationFillMode: "forwards"
-  };
 
   useEffect(() => {
     document.addEventListener('keydown', escKeyListener)
-    document.addEventListener('mousedown', outboundOnClickListener)
+    // document.addEventListener('mousedown', outboundOnClickListener)
 
     return () => {
       document.removeEventListener('keydown', escKeyListener)
-      document.removeEventListener('mousedown', outboundOnClickListener)
+      // document.removeEventListener('mousedown', outboundOnClickListener)
     }
   }, [])
 
@@ -26,14 +21,12 @@ function Modal(props) {
     }
   }
 
-  const outboundOnClickListener = function(e) {
-    if (modalRef.current && !modalRef.current.contains(e.target)) {
-      props.close()
-    }
-  }
-
-  // todo, try different click outside to close
-  // todo, create dropdown menu
+  // we can add the close function to the backdrop instead of a event listener
+  // const outboundOnClickListener = function(e) {
+  //   if (modalRef.current && !modalRef.current.contains(e.target)) {
+  //     props.close()
+  //   }
+  // }
 
   // 如果使用 transition，就不能使用 conditional rendering
   // 而且 transition and display hide 也有很多问题，如果不用display hide，需要把modal藏起来
@@ -45,10 +38,10 @@ function Modal(props) {
   if (!props.show) return null
   return (
   	<div className={`modal-container ${props.containerClassName}`} id="modal-container">
-	    <div className={`modal ${props.modalClassName}`} style={props.show ? mountedStyle : unmountedStyle} ref={modalRef} id="modal">
+	    <div className={`modal ${props.show ? "modal-animation-open" : "modal-animation-close" } ${props.modalClassName}`} ref={modalRef} id="modal">
 	    	{props.children}
 	    </div>
-      <div className={`modal-overlay`}></div>
+      <div className={`modal-overlay`} onClick={() => props.close()}></div>
     </div>
   );
 }
