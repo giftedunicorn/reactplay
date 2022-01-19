de63c3b000ecc9da49fef644205734e8898f97e1
ednew2018 <shenjian8628@gmail.com>
2022-01-19 17:29:09 +0800

    Try diff scroll

--- src/components/ScrollLoad.js
+++ src/components/ScrollLoad.js
@@ -1,10 +1,21 @@
-import { useState, useEffect, createRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 const ScrollLoad = ({ children }) => {
   const [loading, setLoading] = useState(true);
-  const ref = createRef();
+  const ref = useRef(null);
   
   useEffect(() => {
+    // const observer = createObserver()
+    // return () => {
+    //   observer.disconnect();
+    // }
+    document.addEventListener('scroll', scrollToPosition)
+    return () => {
+      document.removeEventListener('scroll', scrollToPosition)
+    }
+  }, []);
+
+  const createObserver = function() {
     const observer = new IntersectionObserver((entries, observer) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
@@ -16,24 +27,34 @@ const ScrollLoad = ({ children }) => {
         }
       });
     });
-    observer.observe(ref.current);
+    let node = ref.current
+    observer.observe(node);
+    return observer
+  }
 
-    return () => {
-      observer.disconnect();
-    }
-  }, []);
+  const scrollToPosition = function(e) {
+    let node = ref.current
+    if (!node) return null
 
-  const scrollToPosition = function(node) {
-    if (node) {
-      
+    const viewPortHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight 
+    const top = node.getBoundingClientRect() && node.getBoundingClientRect().top
+
+    if (top <= viewPortHeight) {
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000)
+    } else {
+      setLoading(true)
     }
   }
 
   return (
-    <div style={{marginTop: 1000}} ref={ref}>
+    <div style={{marginTop: 1000}}>
+      <div ref={ref}>
       {
         loading ? 'Loading...' : children
       }
+      </div>
     </div>
   )
 }
