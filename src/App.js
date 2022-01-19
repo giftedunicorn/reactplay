bc39bd9b24dad4b99ed7144593ac4f4ebd0aee8f
ednew2018 <shenjian8628@gmail.com>
2022-01-19 23:41:40 +0800

    Use react router

--- src/App.js
+++ src/App.js
@@ -1,149 +1,27 @@
-import { useState } from 'react'
-import logo from './assets/logo.svg';
-import Modal from './components/Modal.js';
-import Dropdown from './components/Dropdown.js';
-import Danmu from './components/Danmu.js';
-import ScrollLoad from './components/ScrollLoad.js';
-import './styles/App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from './Home';
+import Layout from './Layout';
 
-function App() {
-  let [showModal, setShowModal] = useState(false)
-  let [email, setEmail] = useState('')
-  let [password, setPassword] = useState('')
-  let [isSignin, setIsSignin] = useState(true)
-
-  const openModal = function() {
-     setShowModal(true)
-  }
-
-  const closeModal = function() {
-     setShowModal(false)
-  }
-
-  const emailOnChange = function(e) {
-  setEmail(e.target.value)
-  }
-
-  const passwordOnChange = function(e) {
-  setPassword(e.target.value)
-  }
-
-  const switchOnClick = function() {
-    setIsSignin(!isSignin)
-  }
-
-  const signinOnClick = function() {
-    let data = {
-      email,
-      password
-    }
-    console.log('signinOnClick', data)
-    // signin(data)
-    // .then(() => {})
-    // .catch()
-  }
-
-  const signupOnClick = function() {
-    let data = {
-      email,
-      password
-    }
-    console.log('signupOnClick', data)
-    // signup(data)
-    // .then(() => {})
-    // .catch()
-  }
-
-  const renderModal = function() {
-    return (
-      <Modal 
-        style={{
-          // custom styles
-        }} 
-        modalClassName={''}
-        containerClassName={''}
-        show={showModal} 
-        open={openModal} 
-        close={closeModal}
-      >
-        <div>{isSignin ? '登录' : '注册'}</div>
-        <input value={email} onChange={(e) => {
-          emailOnChange(e)
-        }} placeholder='邮箱'/>
-        <input value={password} onChange={(e) => {
-          passwordOnChange(e)
-        }} placeholder='密码'/>
-        <button onClick={() => {
-          if (isSignin) signinOnClick()
-          else signupOnClick()
-        }}>提交</button>
-        <button onClick={() => {
-          switchOnClick()
-        }}>{isSignin ? '现在注册' : '现在登录'}</button>
-        <button onClick={() => {
-          closeModal()
-        }}>关闭</button>
-      </Modal>
-    )
-  }
-
-  const renderScroll = function() {
-    return (
-      <ScrollLoad>
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-      </ScrollLoad>
-    )
-  }
-
-  const renderDemo = function() {
-    return (
-      <div>
-        <p>I am snow</p>
-        <p>I am snow</p>
-        <p>I am snow</p>
-      </div>
-    )
-  }
-
-  const renderDropdown = function() {
-    return (
-      <Dropdown />
-    )
-  }
-
-  const renderDanmu = function() {
-    return (
-      <Danmu />
-    )
-  }
-
+export default function App() {
   return (
-    <div className="App">
-      <div style={{margin: 100}}>
-        <button onClick={() => {
-          openModal()
-        }}>Open</button>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/layout">Layout</Link>
+            </li>
+          </ul>
+        </nav>
+		<Routes>
+			<Route path="/" element={<Home />} />
+		<Route path="Layout" element={<Layout />} />
+		</Routes>
       </div>
-      {renderDemo()}
-      {renderDropdown()}
-      {renderDanmu()}
-      {renderScroll()}
-      {renderModal()}
-    </div>
+    </Router>
   );
 }
-
-export default App;
-
-// <Modal 
-//   show={Boolean} 
-//   className={""}
-//   style={""} 
-//   containerClassName={""}
-//   containerClose={boolean}
-//   escClose={boolean}
-//  />
-
-// dong hua transparent
---
+++ src/Home.js
@@ -0,0 +1,149 @@
+import { useState } from 'react'
+import logo from './assets/logo.svg';
+import Modal from './components/Modal.js';
+import Dropdown from './components/Dropdown.js';
+import Danmu from './components/Danmu.js';
+import ScrollLoad from './components/ScrollLoad.js';
+import './styles/App.css';
+
+function App() {
+  let [showModal, setShowModal] = useState(false)
+  let [email, setEmail] = useState('')
+  let [password, setPassword] = useState('')
+  let [isSignin, setIsSignin] = useState(true)
+
+  const openModal = function() {
+     setShowModal(true)
+  }
+
+  const closeModal = function() {
+     setShowModal(false)
+  }
+
+  const emailOnChange = function(e) {
+  setEmail(e.target.value)
+  }
+
+  const passwordOnChange = function(e) {
+  setPassword(e.target.value)
+  }
+
+  const switchOnClick = function() {
+    setIsSignin(!isSignin)
+  }
+
+  const signinOnClick = function() {
+    let data = {
+      email,
+      password
+    }
+    console.log('signinOnClick', data)
+    // signin(data)
+    // .then(() => {})
+    // .catch()
+  }
+
+  const signupOnClick = function() {
+    let data = {
+      email,
+      password
+    }
+    console.log('signupOnClick', data)
+    // signup(data)
+    // .then(() => {})
+    // .catch()
+  }
+
+  const renderModal = function() {
+    return (
+      <Modal 
+        style={{
+          // custom styles
+        }} 
+        modalClassName={''}
+        containerClassName={''}
+        show={showModal} 
+        open={openModal} 
+        close={closeModal}
+      >
+        <div>{isSignin ? '登录' : '注册'}</div>
+        <input value={email} onChange={(e) => {
+          emailOnChange(e)
+        }} placeholder='邮箱'/>
+        <input value={password} onChange={(e) => {
+          passwordOnChange(e)
+        }} placeholder='密码'/>
+        <button onClick={() => {
+          if (isSignin) signinOnClick()
+          else signupOnClick()
+        }}>提交</button>
+        <button onClick={() => {
+          switchOnClick()
+        }}>{isSignin ? '现在注册' : '现在登录'}</button>
+        <button onClick={() => {
+          closeModal()
+        }}>关闭</button>
+      </Modal>
+    )
+  }
+
+  const renderScroll = function() {
+    return (
+      <ScrollLoad>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </ScrollLoad>
+    )
+  }
+
+  const renderDemo = function() {
+    return (
+      <div>
+        <p>I am snow</p>
+        <p>I am snow</p>
+        <p>I am snow</p>
+      </div>
+    )
+  }
+
+  const renderDropdown = function() {
+    return (
+      <Dropdown />
+    )
+  }
+
+  const renderDanmu = function() {
+    return (
+      <Danmu />
+    )
+  }
+
+  return (
+    <div className="App">
+      <div style={{margin: 100}}>
+        <button onClick={() => {
+          openModal()
+        }}>Open</button>
+      </div>
+      {renderDemo()}
+      {renderDropdown()}
+      {renderDanmu()}
+      {renderScroll()}
+      {renderModal()}
+    </div>
+  );
+}
+
+export default App;
+
+// <Modal 
+//   show={Boolean} 
+//   className={""}
+//   style={""} 
+//   containerClassName={""}
+//   containerClose={boolean}
+//   escClose={boolean}
+//  />
+
+// dong hua transparent
