9649b9620d31a42c24487759aad6d50164b9090d
ednew2018 <shenjian8628@gmail.com>
2022-01-21 12:11:19 +0800

    Add more

--- src/Layout.js
+++ src/Layout.js
@@ -2,19 +2,36 @@ import { useState } from 'react'
 import './styles/Layout.css';
 
 function Layout() {
-  const renderCenter = function() {
+  const targetOnClick = function(e) {
+    let target = e.target
+    let currentTarget = e.currentTarget
+    console.log("target", target)  // li
+    console.log("currentTarget", currentTarget)  // li
+    console.log(target === currentTarget)  // true
+  }
+
+  // learn event current target vs target
+  const renderCurrentTarget = function() {
     return (
       <div>
+        测试currentTarget and target，两个会因为情况不同而指向不同的对象
+        <ul onClick={(e) => targetOnClick(e)}>
+          <li>hello 1</li>
+          <li>hello 2</li>
+          <li>hello 3</li>
+          <li>hello 4</li>
+        </ul>
       </div>
     )
   }
 
+  // learn css position
   const renderPosition = function() {
     return (
       <div id="relative" className="positionContainer">
-        <div class="sticky">stikcy head</div>
-        <h2 class="absolute">这是一个absolute的标题</h2>
-        <h2 class="fixed">这是一个fixed的标题</h2>
+        <div className="sticky">stikcy head</div>
+        <h2 className="absolute">这是一个absolute的标题</h2>
+        <h2 className="fixed">这是一个fixed的标题</h2>
         <p>用relative, 相对于自己的位置来移动，比如 left -20，意思就是往自己左边移动20。一般用来作为absolute的容器。</p>
         <p>用absolute,一个元素可以放在页面上的任何位置，相对于父级relative元素，如果没有则html元素。</p>
         <p>fixed,一个元素可以放在视窗上的任何位置，跟着浏览器走的，而abs是跟着relative元素走的。</p>
@@ -23,18 +40,37 @@ function Layout() {
     )
   }
 
+  // learn how to make a left center right layout
   const renderLeftRight = function() {
     return (
-      <div>
+      <div className="container leftRightContainer">
+        <div className="leftItem">left</div>
+        <div className="centerItem">center</div>
+        <div className="rightItem">right</div>
       </div>
     )
   }
 
-  const renderHeadFoot = function() {
+  // learn how to center a item
+  const renderCenter = function() {
     return (
-      <div>
+      <div className="container center">
+        <div className="box">
+          <div>center</div>
+        </div>
+      </div>
+    )
+  }
+
+  // learn how to make head body foot layout
+  const renderHeadFoot = function() {
+    const items = Array(100).fill(0).map((item, index) => <div key={`${index}-body`}>{index} body</div>)
+    const text = "body"
+
+    return (
+      <div className="container headFootcontainer">
         <div className="head">head</div>
-        <div className="body">body</div>
+        <div className="body">{text}</div>
         <div className="foot">foot</div>
       </div>
     )
@@ -43,9 +79,10 @@ function Layout() {
   return (
     <div className="layout">
       {renderPosition()}
+      {renderCurrentTarget()}
+      {renderLeftRight()}
       {renderCenter()}
       {renderHeadFoot()}
-      {renderLeftRight()}
     </div>
   );
 }
