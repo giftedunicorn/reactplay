8eaccd6c082b67a3616947510837b93af3beeaef
ednew2018 <shenjian8628@gmail.com>
2022-01-21 15:54:23 +0800

    Add more to center

--- src/Center.js
+++ src/Center.js
@@ -3,11 +3,66 @@ import './styles/Center.css';
 
 function Center() {
   // learn how to center a item
-  const renderCenter = function() {
+  const renderCenterFlex = function() {
     return (
-      <div className="container center">
-        <div className="centerBox">
-          <div>center</div>
+      <div className="container centerFlex">
+        <div className="centerBoxFlex">
+          <div>center flex</div>
+        </div>
+      </div>
+    )
+  }
+
+  const renderCenterGrid = function() {
+    return (
+      <div className="container centerGrid">
+        <div className="centerBoxGrid">
+          <div>center grid</div>
+        </div>
+      </div>
+    )
+  }
+
+
+  const renderCenterTransform = function() {
+    return (
+      <div className="container centerTransform">
+        <div className="centerBoxTransform">
+          <div>center transform</div>
+        </div>
+      </div>
+    )
+  }
+
+  const renderCenterMargin = function() {
+    return (
+      <div className="container centerMargin">
+        <div className="centerBoxMargin">
+          <div>center margin</div>
+        </div>
+      </div>
+    )
+  }
+
+  const renderCenterTable = function() {
+    return (
+      <div className="container">
+        <div className="centertableOuter">
+          <div className="centertableMiddle">
+            <div className="centerBoxTable centertableInner">
+              <div>center table</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  const renderCenterInline = function() {
+    return (
+      <div className="container centerInline">
+        <div className="centerBoxInline">
+          <div>center inline</div>
         </div>
       </div>
     )
@@ -15,7 +70,12 @@ function Center() {
 
   return (
     <div className="layout">
-      {renderCenter()}
+      {renderCenterFlex()}
+      {renderCenterGrid()}
+      {renderCenterTransform()}
+      {renderCenterMargin()}
+      {renderCenterTable()}
+      {renderCenterInline()}
     </div>
   );
 }
